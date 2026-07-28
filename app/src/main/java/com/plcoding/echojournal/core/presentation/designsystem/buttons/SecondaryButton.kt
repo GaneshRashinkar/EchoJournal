7cc4a95be88b1849c873973496b94c7ac87a4918
Ganesh Rashinkr <ganeshrashinkar007@gmail.com>
2026-07-28 22:51:43 +0530

    6.1 Screen skeleton

--- app/src/main/java/com/plcoding/echojournal/core/presentation/designsystem/buttons/SecondaryButton.kt
+++ app/src/main/java/com/plcoding/echojournal/core/presentation/designsystem/buttons/SecondaryButton.kt
@@ -1,26 +1,13 @@
 package com.plcoding.echojournal.core.presentation.designsystem.buttons
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
 import com.plcoding.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
-import com.plcoding.echojournal.core.presentation.designsystem.theme.buttonGradient
 
 
 @Composable
@@ -34,7 +21,7 @@ fun SecondaryButton(
         onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.onPrimaryContainer),
-
+        modifier = modifier
         ) {
 
         Text(
